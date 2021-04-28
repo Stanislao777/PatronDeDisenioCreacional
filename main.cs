8a2493c2ab1412e2ab913688bd2ae83f0e815c00
Stanislao Vargas <>
2021-04-28 23:42:24 +0000

    final menu

--- main.cs
+++ main.cs
@@ -15,13 +15,36 @@ class MainClass
 	public static void Main (string[] args)
 	{
 		CocheFactory cocheFactory = getCocheFactory(2020);
-		Console.WriteLine("anio: {0}", cocheFactory.obtenerAnioModelo());
+		Console.WriteLine("////////////////////////");
+		Console.WriteLine("////   anio: {0}   ////", cocheFactory.obtenerAnioModelo());
+		Console.WriteLine("////////////////////////");
 
 		Chasis chasis = cocheFactory.fabricarChasis();
+		
 		Console.WriteLine("Peso del chasis: {0}", chasis.getPeso());
 		
 		Console.WriteLine("Material del chasis: {0}", chasis.getMaterial());
 
 		Console.WriteLine("Clasificacion del chasis: {0}", chasis.getClasificacion());
+
+		Console.WriteLine("==============================");
+
+		Rueda rueda = cocheFactory.fabricarRueda();
+
+		Console.WriteLine("Diametro de la rueda: {0}", rueda.getDiametro());
+		
+		Console.WriteLine("Indice de cargas: {0}", rueda.getIndiceDeCarga());
+
+		Console.WriteLine("Indice de velocidad: {0}", rueda.getIndiceDeVelocidad());
+
+		Console.WriteLine("==============================");
+
+		Motor motor = cocheFactory.fabricarMotor();
+
+		Console.WriteLine("Tipo de rueda: {0}", motor.getTipo());
+
+		Console.WriteLine("Cantidad de cilindros: {0}", motor.getCantidadCilindros());
+
+		Console.WriteLine("Disposicion de cilindros: {0}", motor.getDisposicionCilindros());
 	}	
 }
